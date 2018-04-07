665072493f253ed5acfc1c4e0a9394934b6345ab
uuuu <kolos181@gmail.com>
2018-04-07 15:51:42 +0300

    removed comments wrong import

--- src/main/java/com/example/MedBook/CommentRepository.java
+++ src/main/java/com/example/MedBook/CommentRepository.java
@@ -3,12 +3,11 @@ package com.example.MedBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.xml.stream.events.Comment;
 
 /**
  * Created by uuuu on 4/4/2018.
  */
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comments, Long> {
 }
