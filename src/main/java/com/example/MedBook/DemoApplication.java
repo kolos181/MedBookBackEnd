33cc7bdbd21c2030576059b9a97923cd6daaaffc
uuuu <kolos181@gmail.com>
2018-04-09 14:25:50 +0300

    reconfigured project according to spring boot requrements

--- src/main/java/com/example/MedBook/DemoApplication.java
+++ src/main/java/com/example/MedBook/DemoApplication.java
@@ -2,61 +2,11 @@ package com.example.MedBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
 
-import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        try {
-
-            Class.forName("org.postgresql.Driver");
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-            String url = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
-
-            String user = dbUri.getUserInfo().split(":")[0];
-            String pass = dbUri.getUserInfo().split(":")[1];
-
-            System.out.println(dbUri.getHost());
-            System.out.println(dbUri.getPath());
-            System.out.println(dbUri.getUserInfo().split(":")[0]);
-            System.out.println(dbUri.getUserInfo().split(":")[1]);
-
-
-            Connection con = DriverManager.getConnection(
-                    url, user, pass);
-            Statement stmt = con.createStatement();
-//            ResultSet rs = stmt.executeQuery("CREATE TABLE IF NOT EXISTS patients (ID SERIAL PRIMARY KEY, " +
-//                    "first_name text NOT NULL, " +
-//                    "last_name text NOT NULL," +
-//                    "sex text," +
-//                    "birth_date DATE," +
-//                    "state text," +
-//                    "address text)");
-//
-            stmt.executeQuery("CREATE SEQUENCE hibernate_sequence START 1");
-
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-
-
         SpringApplication.run(DemoApplication.class, args);
     }
 }
--- src/main/java/com/example/MedBook/HomeController.java
+++ src/main/java/com/example/MedBook/HomeController.java
@@ -1,21 +1,18 @@
 package com.example.MedBook;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Timestamp;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by uuuu on 4/4/2018.
  */
 
-@Controller
-@RequestMapping("/notNow")
+@RestController
+@RequestMapping("/")
 public class HomeController {
 
     private final PatientRepository patientRepository;
@@ -23,27 +20,15 @@ public class HomeController {
     private final CommentRepository commentRepository;
 
     @Autowired
-    PatientService patientService;
-
-    @Autowired
     public HomeController(PatientRepository patientRepository, CommentRepository commentRepository) {
         this.patientRepository = patientRepository;
         this.commentRepository = commentRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String showPatients(ModelMap modelMap) {
-        patientService.addPatient(new Patient("Elder", "Galmeister", "Male",
-                new Date(1L), "Utah", "Bob st. 1"));
-        patientService.addPatient(new Patient("Elder", "Galmeister", "Male",
-                new Date(1L), "Utah", "Bob st. 1"));
-        patientService.addPatient(new Patient("Elder", "Galmeister", "Male",
-                new Date(1L), "Utah", "Bob st. 1"));
-        patientService.addPatient(new Patient("Elder", "Galmeister", "Male",
+    @GetMapping("/addPatient")
+    public Patient showPatients() {
+        return patientRepository.save(new Patient("Elder", "Galmeister", "Male",
                 new Date(1L), "Utah", "Bob st. 1"));
 
-//        List<Patient> patientList = patientRepository.findAll();
-//        modelMap.addAttribute("patients", patientList);
-        return "frontPage";
     }
 }
