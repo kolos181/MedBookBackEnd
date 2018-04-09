09090f79c3d8d642702f2aa010b3f2b7b0479e3d
uuuu <kolos181@gmail.com>
2018-04-09 14:52:58 +0300

    changed GetMapping to PostMapping

--- src/main/java/com/example/MedBook/HomeController.java
+++ src/main/java/com/example/MedBook/HomeController.java
@@ -1,9 +1,7 @@
 package com.example.MedBook;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.Optional;
@@ -26,7 +24,7 @@ public class HomeController {
         this.commentRepository = commentRepository;
     }
 
-    @GetMapping("/addPatient")
+    @PostMapping("/addPatient")
     public Patient addPatient() {
         return patientRepository.save(new Patient("Elder", "Galmeister", "Male",
                 new Date(1L), "Utah", "Bob st. 1"));
